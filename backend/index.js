9b125d450882fd9ffa3edc6dfea195ce7c86512a
igor <igor8280@gmail.com>
2018-05-28 11:24:54 +0200

    node server up

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,7 @@
-import http from 'http';
+const http = require('http');
+
+http.createServer((req, res) => {
+	res.writeHead(200, {'Content-Type': 'text/html'});
+	res.write('Test msg');
+	res.end();
+}).listen(8280);
